3584ad22b5082efec3de601251407d3e2cfb714f
Solo-Dev0 <sreemanikandan.software@gmail.com>
2024-04-13 19:53:32 +0530

    Initial Commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+     <p className="text-3xl font-bold text-indigo-500">
+      Lincord
+     </p> 
+     <Button>
+      Clickme
+     </Button>
+    </div>
+  )
+}
